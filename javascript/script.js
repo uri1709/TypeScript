a878c3380567787b3256a2ddabe80179a17a391f
uri1709 <uri1709k@gmail.com>
2024-04-30 15:22:32 +0600

    urok2
    urok2

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,13 @@
 "use strict";
-console.log('1');
-console.log(5);
-let n = 4;
-console.log(n);
+const a1 = "Hello TS!!!";
+document.querySelector('.out-1').textContent = a1;
+const a2 = document.querySelector('.out-2').textContent;
+console.log(a2);
+const a3 = document.querySelector('.out-3').textContent;
+console.log(a3, typeof a3);
+// const i5 = <HTMLInputElement> document.querySelector('.i-5');
+const i5 = document.querySelector('.i-5');
+console.log(i5.value);
+const i6 = document.querySelector('.i-6');
+console.log(+i6.value);
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -1,6 +1,16 @@
-console.log('1');
-console.log(5);
+const a1: string = "Hello TS!!!";
+document.querySelector('.out-1').textContent = a1;
 
-let n:number = '4';
-// n = 'f';
-console.log(n);
+const a2: string = document.querySelector('.out-2').textContent;
+console.log(a2);
+
+const a3: string = document.querySelector('.out-3').textContent;
+console.log(a3, typeof a3);
+
+// const i5 = <HTMLInputElement> document.querySelector('.i-5');
+const i5 = document.querySelector('.i-5') as HTMLInputElement;
+console.log(i5.value);
+
+
+const i6 = document.querySelector('.i-6') as HTMLInputElement;
+console.log(+i6.value);
