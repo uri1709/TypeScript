72f24afe05ab0fee44a42b430a9c80d87c50d081
uri1709 <uri1709k@gmail.com>
2024-04-30 14:57:51 +0600

    ts urok 1
    ts urok 1

---
+++ javascript/script.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log('1');
+console.log(5);
+let n = 4;
+console.log(n);
+//# sourceMappingURL=script.js.map
---
+++ script.ts
@@ -0,0 +1,6 @@
+console.log('1');
+console.log(5);
+
+let n:number = '4';
+// n = 'f';
+console.log(n);
